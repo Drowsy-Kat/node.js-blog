dd669d6c73b9b737e172a8eb627b8aa689aaed3f
Drowsy-Kat <khizer78901@gmail.com>
2024-05-04 22:42:03 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const articleRouter = require('./routes/articles');
+const mongoose = require('mongoose');
+const Article = require('./models/articles')
+const methodOverride = require('method-override')
+const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/blog')
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false}))
+app.use(methodOverride('_method'))
+
+
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc'})
+  res.render('articles/index', {articles: articles})
+})
+
+app.listen(5000)
+
+app.use('/articles', articleRouter)
